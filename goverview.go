0811048a4f46fbc103b95b2d69cb4c03ed57abe7
Emil Petersson <git@senil.io>
2019-04-11 20:42:08 +0200

    moved login URL to env

--- goverview.go
+++ goverview.go
@@ -3,104 +3,119 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
 
-    "github.com/vmware/govmomi/examples"
-    "github.com/vmware/govmomi/view"
-    "github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/mo"
 )
 
 func main() {
-    ctx := context.Background()
+	ctx := context.Background()
 
-    // Connect and login to ESX or vCenter
-    c, err := examples.NewClient(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer c.Logout(ctx)
+	// Creating a connection context
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-    // Create view of VirtualMachine objects
-    mv := view.NewManager(c.Client)
-    v, err := mv.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"VirtualMachine"}, true)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer v.Destroy(ctx)
+	// Parse URL from environment
+	u := os.Getenv("GOVERVIEW_URL")
 
-    // Create view of HostSystem objects
-    mh := view.NewManager(c.Client)
-    h, err := mh.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer h.Destroy(ctx)
+	url, err := url.Parse(u)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
 
-    // Retrieve properties for all machines
-    var vms []mo.VirtualMachine
-    var vmprops = []string{
-        "name",
-        "snapshot",
-        "layoutEx.file",
-        "guest.hostName",
-        "config.guestFullName",
-        "summary.quickStats.overallCpuUsage",
-        "summary.quickStats.uptimeSeconds",
-        "summary.customValue",
-        "summary.quickStats",
-        "config.hardware.memoryMB",
-        "config.hardware.numCPU",
-        "config.hardware.device",
-        "config.annotation",
-        "summary.runtime.powerState",
-        "runtime.host",
-        "network",
-        "guest.toolsRunningStatus",
-        "guest.toolsVersionStatus",
-        "guest.net",
-        "guest.guestState",
-        "config.guestId",
-        "config.version",
-        "runtime",
-        "config",
-    }
+	// Connecting to vCenter
+	c, err := govmomi.NewClient(ctx, url, true)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
 
-    err = v.Retrieve(ctx, []string{"VirtualMachine"}, vmprops, &vms)
+	// Create view of VirtualMachine objects
+	mv := view.NewManager(c.Client)
+	v, err := mv.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"VirtualMachine"}, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer v.Destroy(ctx)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Create view of HostSystem objects
+	mh := view.NewManager(c.Client)
+	h, err := mh.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"HostSystem"}, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer h.Destroy(ctx)
 
-    // Retrieve properties for hosts
-    var hosts []mo.HostSystem
-    var hostprops = []string{
-        "summary.hardware.cpuModel",
-        "config.product.fullName",
-        "hardware.cpuInfo.numCpuCores",
-        "hardware.cpuInfo.hz",
-        "hardware.cpuInfo.numCpuThreads",
-        "hardware.cpuInfo.numCpuPackages",
-        "hardware.memorySize",
-        "hardware.systemInfo.model",
-        "hardware.systemInfo.vendor",
-        "hardware.biosInfo.biosVersion",
-        "name",
-        "summary.hardware.cpuModel",
-        "summary.hardware.numCpuPkgs",
-        "summary.hardware.numNics",
-        "summary.overallStatus",
-    }
+	// Retrieve properties for all machines
+	var vms []mo.VirtualMachine
+	var vmprops = []string{
+		"name",
+		"snapshot",
+		"layoutEx.file",
+		"guest.hostName",
+		"config.guestFullName",
+		"summary.quickStats.overallCpuUsage",
+		"summary.quickStats.uptimeSeconds",
+		"summary.customValue",
+		"summary.quickStats",
+		"config.hardware.memoryMB",
+		"config.hardware.numCPU",
+		"config.hardware.device",
+		"config.annotation",
+		"summary.runtime.powerState",
+		"runtime.host",
+		"network",
+		"guest.toolsRunningStatus",
+		"guest.toolsVersionStatus",
+		"guest.net",
+		"guest.guestState",
+		"config.guestId",
+		"config.version",
+		"runtime",
+		"config",
+	}
 
-    err = h.Retrieve(ctx, []string{"HostSystem"}, hostprops, &hosts)
+	err = v.Retrieve(ctx, []string{"VirtualMachine"}, vmprops, &vms)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // fmt.Println(vms)
-    for _, vm := range vms {
-        fmt.Printf("%s\t%s\t%s\t%d\t%s\n", vm.Name, vm.Config.GuestFullName, vm.Config.GuestId, vm.Config.Hardware.MemoryMB, vm.Config.Annotation)
-    }
+	// Retrieve properties for hosts
+	var hosts []mo.HostSystem
+	var hostprops = []string{
+		"summary.hardware.cpuModel",
+		"config.product.fullName",
+		"hardware.cpuInfo.numCpuCores",
+		"hardware.cpuInfo.hz",
+		"hardware.cpuInfo.numCpuThreads",
+		"hardware.cpuInfo.numCpuPackages",
+		"hardware.memorySize",
+		"hardware.systemInfo.model",
+		"hardware.systemInfo.vendor",
+		"hardware.biosInfo.biosVersion",
+		"name",
+		"summary.hardware.cpuModel",
+		"summary.hardware.numCpuPkgs",
+		"summary.hardware.numNics",
+		"summary.overallStatus",
+	}
+
+	err = h.Retrieve(ctx, []string{"HostSystem"}, hostprops, &hosts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(vms)
+	for _, vm := range vms {
+		fmt.Printf("%s\t%s\t%s\t%d\t%s\n", vm.Name, vm.Config.GuestFullName, vm.Config.GuestId, vm.Config.Hardware.MemoryMB, vm.Config.Annotation)
+	}
 }
